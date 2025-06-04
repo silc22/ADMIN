ac775744ff9c42bd7ff26b1d25f1260ef9dfe8f4
silc22 <silcerquatti@gmail.com>
2025-06-04 11:23:51 +0200

    Style tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      // Aquí iremos luego a añadir tu paleta de colores
+      colors: {
+        // Colores base
+        white: "#ffffff",
+        black: "#000000",
+
+        // Presupuestos: pendientes
+        pending: {
+          DEFAULT: "#ffe190",
+          light: "#fff9eb"
+        },
+        // Presupuestos: rechazados
+        rejected: {
+          DEFAULT: "#eef0ff",
+          light: "#a2a6f0"
+        },
+        // Presupuestos: aprobados
+        approved: {
+          DEFAULT: "#e7fdf6",
+          light: "#73d8d1"
+        },
+        // Botones
+        btn: {
+          success: "#69e562",
+          primary: "#4298f8",
+          danger: "#dc3545"
+        }
+      }
+    }
+  },
+  plugins: []
+};
+
